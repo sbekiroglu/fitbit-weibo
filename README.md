811ae908d7a166fad39d0135025c76c004643ae5
luolei <meleilei@gmail.com>
2013-08-11 23:10:45 +0800

    twitter test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ###fitbit分享到微博
 基于node.js的自动分享fitbit数据到新浪微博的程序
+详情和交流请联系 [@foru17](https://twitter.com/foru17)
---
+++ server.js
@@ -0,0 +1,40 @@
+var http=require('http');
+var sys=require("sys"),
+    url=require('url'),
+    path=require('path'),
+    fs=require('fs');
+
+sys.puts('hello world');
+
+http.createServer(function(request,response){
+    var uri=url.parse(request.url).pathname;
+    var filename=path.join(process.cwd(),uri);
+    fs.exists(filename,function(exists){
+        if(!exists){
+            response.writeHead(404,{
+                'Content-Type':'text/plain'
+            });
+            response.write('404 Not Found\n');
+            response.end();
+            return;
+        }
+    })
+
+    fs.readFile(filename,'binary',function(err,file){
+        if(err){
+            response.writeHead(500,{
+                'Content-Type':'text/plain'
+            });
+            response.write(err+"\n");
+            response.end();
+            return;
+        }
+    })
+
+    response.writeHead(200,{
+        'Content-Type':'text/html'
+    });
+    response.end('hello fitbit');
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124');
